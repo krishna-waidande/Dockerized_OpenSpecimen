c3e36f8835c2f17743a74b9b8a46e9525b5a63f3
krishna-waidande <krishnawaidande105@gmail.com>
2019-01-29 19:50:38 +0530

    Added proper comment messages for each line.

--- openspecimen_version2/entry-point.sh
+++ openspecimen_version2/entry-point.sh
@@ -10,13 +10,20 @@ sed -i "s/<driver_class>/$driver_class/g" /usr/local/tomcat/conf/context.xml
 sed -i "s/<database_port>/$db_port/g" /usr/local/tomcat/conf/context.xml
 sed -i "s/<database_name>/$db_name/g" /usr/local/tomcat/conf/context.xml
 
-
+#Removing the unessasary files from webapps directory.
 rm -r /usr/local/tomcat/webapps/ROOT 
 rm -r /usr/local/tomcat/webapps/docs 
-rm -r /usr/local/tomcat/webapps/examples 
-ls /usr/local/tomcat/webapps
+rm -r /usr/local/tomcat/webapps/examples
+rm -r /usr/local/tomcat/host-manager
+rm -r /usr/local/tomcat/manager
+
+#Creating data and plugin directory for OpenSpecimen.
 mkdir -p /usr/local/openspecimen/os-data 
 mkdir -p /usr/local/openspecimen/os-plugins
+
+#Starting the Tomcat
 /usr/local/tomcat/bin/catalina.sh start
 sleep 15
+
+#Displaying the Tomcat logs as well as OpenSpecimen logs.
 tail -f /usr/local/tomcat/logs/catalina.out -f /usr/local/openspecimen/os-data/logs/os.log
