21db27815ee6ce9d8cf277f81fa4cf7e9db63eb8
krishna-waidande <krishnawaidande105@gmail.com>
2019-02-14 22:33:58 +0530

    Renamed the property file variables.

--- openspecimen_version2/entry-point.sh
+++ openspecimen_version2/entry-point.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
 ensureProperties() {
-  if [ -z "$DB_USERNAME" ]; then
-    echo "Error: database user not specified. Please specify the value for DB_USERNAME in env.list file.";
+  if [ -z "$db_user" ]; then
+    echo "Error: database user not specified. Please specify the value for db_user in config.properties file.";
     exit 1;
   fi
 
-  if [ -z "$DB_PASSWORD" ]; then
-    echo "Error: database password not specified. Please specify the value for DB_PASSWORD in env.list";
+  if [ -z "$db_passwd" ]; then
+    echo "Error: database password not specified. Please specify the value for db_passwd in config.properties";
     exit 1;
   fi
 
-  if [ -z "$DB_HOST" ]; then
-    echo "Error: db_host not specified. Please specify the value for DB_HOST in env.list";
+  if [ -z "$db_host" ]; then
+    echo "Error: db_host not specified. Please specify the value for db_host in config.properties";
     exit 1;
   fi
 
-  if [ -z "$DRIVER_CLASS" ]; then
-    echo "Error: driver class not specified. Please specify the value for DRIVER_CLASS in env.list";
+  if [ -z "$db_type" ]; then
+    echo "Error: driver class not specified. Please specify the value for db_type in config.properties";
     exit 1;
   fi
 
-  if [ -z "$DB_PORT" ]; then
-    echo "Error: database port not specified. Please specify the value for DB_PORT in env.list";
+  if [ -z "$db_port" ]; then
+    echo "Error: database port not specified. Please specify the value for DB_PORT in config.properties";
     exit 1;
   fi
 
-  if [ -z "$DB_NAME" ]; then
-    echo "Error: database name not specified. Please specify the value for DB_NAME in env.list";
+  if [ -z "$db_name" ]; then
+    echo "Error: database name not specified. Please specify the value for DB_NAME in config.properties";
     exit 1;
   fi
 }
 
 configureContextFile() {
   echo "Replacing database values."
-  sed -i "s/<db_username>/$DB_USERNAME/g"   /usr/local/tomcat/conf/context.xml
-  sed -i "s/<db_password>/$DB_PASSWORD/g"   /usr/local/tomcat/conf/context.xml
-  sed -i "s/<database_host>/$DB_HOST/g"     /usr/local/tomcat/conf/context.xml
-  sed -i "s/<driver_class>/$DRIVER_CLASS/g" /usr/local/tomcat/conf/context.xml
-  sed -i "s/<database_port>/$DB_PORT/g"     /usr/local/tomcat/conf/context.xml
-  sed -i "s/<database_name>/$DB_NAME/g"     /usr/local/tomcat/conf/context.xml
+  sed -i "s/<db_username>/$db_user/g"   /usr/local/tomcat/conf/context.xml
+  sed -i "s/<db_password>/$db_passwd/g" /usr/local/tomcat/conf/context.xml
+  sed -i "s/<database_host>/$db_host/g" /usr/local/tomcat/conf/context.xml
+  sed -i "s/<database_port>/$db_port/g" /usr/local/tomcat/conf/context.xml
+  sed -i "s/<database_name>/$db_name/g" /usr/local/tomcat/conf/context.xml
+
+  if [[ "$db_type" = "mysql" || "$db_type" = "MYSQL" ]]; then
+    sed -i "s/<driver_class>/com.mysql.jdbc.Driver/g" /usr/local/tomcat/conf/context.xml
+  fi
 }
 
 removeUnwantedDir() {
@@ -47,15 +50,15 @@ removeUnwantedDir() {
   rm -r /usr/local/tomcat/webapps/ROOT
   rm -r /usr/local/tomcat/webapps/docs
   rm -r /usr/local/tomcat/webapps/examples
-  rm -r /usr/local/tomcat/host-manager
-  rm -r /usr/local/tomcat/manager
+}
+
+createDataAndPluginDir() {
+  echo "Creating data and plugin directory for OpenSpecimen"
+  mkdir -p /usr/local/openspecimen/os-data 
+  mkdir -p /usr/local/openspecimen/os-plugins
 }
 
 startTomcat() {
-  echo "Creating data and plugin directory for OpenSpecimen"
-  mkdir -p /usr/local/openspecimen/os-data
-  mkdir -p /usr/local/openspecimen/os-plugins
-
   echo "Starting the Tomcat"
   /usr/local/tomcat/bin/catalina.sh start
   sleep 10
@@ -68,6 +71,7 @@ main() {
   ensureProperties;
   configureContextFile;
   removeUnwantedDir;
+  createDataAndPluginDir;
   startTomcat;
 }
 
